a2879262aaf183fc24745d45c33a50688b7dc852
Marshay Brown <65259996+newCodeWriter@users.noreply.github.com>
2021-04-14 17:09:32 -0400

    add statement to serve build folder in production;

--- app.js
+++ app.js
@@ -39,29 +39,36 @@ app.use("/api", uploadRouter);
 app.use("/api/checkout", checkoutPayment);
 app.use("/api/itinerary", itineraryRouter);
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(join(__dirname, "client/build")));
+	app.get("*", (req, res) => {
+		res.sendFile(join(__dirname, "client/build", "index.html"));
+	});
+}
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json({ error: err });
+	// render the error page
+	res.status(err.status || 500);
+	res.json({ error: err });
 });
 
 //The line below connects our node server with our mongo database
 const url = process.env.MONGO_URL;
 
 mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
 });
 
 module.exports = app;
